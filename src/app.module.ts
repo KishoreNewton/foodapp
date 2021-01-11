45fd7c2c4c1bd71353dcbf90b5205bde6a4c3dcd
Kishore Newton <kishorenke@gmail.com>
2021-01-12 01:34:01 +0530

    feat: Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { TypeOrmModule } from '@nestjs/typeorm';
