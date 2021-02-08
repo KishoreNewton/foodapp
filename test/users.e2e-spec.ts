8f60ed835be030d3fa6d92d632e3bc2b279c2942
Kishore Newton <kishorenke@gmail.com>
2021-02-09 00:20:36 +0530

    test::e2e: login user

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -6,6 +6,16 @@ import { getConnection } from 'typeorm';
 
 const GRAPHQL_ENDPOINT: '/graphql' = '/graphql';
 
+interface TestUser {
+  email: string;
+  password: string;
+}
+
+const testUser: TestUser = {
+  email: process.env.AWS_SOURCE,
+  password: '12345'
+};
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -17,8 +27,6 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
-  const EMAIL: string = process.env.AWS_SOURCE;
-
   describe('createAccount', () => {
     it('should create an account', async () => {
       return request(app.getHttpServer())
@@ -27,8 +35,8 @@ describe('UserModule (e2e)', () => {
           query: `
             mutation {
               createAccount(input: {
-                email: "${EMAIL}",
-                password: "password",
+                email: "${testUser.email}",
+                password: "${testUser.password}",
                 role: Owner
               }) {
                 ok
@@ -42,6 +50,92 @@ describe('UserModule (e2e)', () => {
           expect(res.body.data.createAccount.ok).toBe(true);
           expect(res.body.data.createAccount.error).toBe(null);
         });
+    });
+
+    it('should fail if account already exists', async () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${testUser.email}",
+              password: "${testUser.password}",
+              role: Owner
+            }) {
+              ok
+              error
+            }
+          }
+      `
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
+
+  describe('login', () => {
+    it('should login with correct credentials', async () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            login(input: {
+              email: "${testUser.email}",
+              password: "${testUser.password}",
+            }) {
+              error
+              token
+              ok
+            }
+          }
+        `
+        })
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: { login }
+            }
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+        });
+    });
+
+    it('should not be able to login with wrong credentials', async () => {
+      return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query: `
+        mutation {
+          login(input: {
+            email: "${testUser.email}",
+            password: "wrong.password",
+          }) {
+            error
+            token
+            ok
+          }
+        }
+      `
+      })
+      .expect(200)
+      .expect(res => {
+        const {
+          body: {
+            data: { login }
+          }
+        } = res;
+        expect(login.ok).toBe(false);
+        expect(login.error).toBe("Wrong password");
+        expect(login.token).toEqual(null);
+      });
     });
   });
 
